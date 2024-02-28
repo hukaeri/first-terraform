f1c653af27d812874a56d19e9cefd1379bb84917
xinyang.li <makeit.li@qq.com>
2024-02-28 17:22:14 +0800

    test5

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,9 @@
 resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
 }
 
 resource "aws_subnet" "main-public-1" {
-  vpc_id                  = aws_vpc.main.id
-  availability_zone       = "eu-west-1a"
-  cidr_block              = "10.0.0.0/16"
+  vpc_id            = aws_vpc.main.id
+  availability_zone = "eu-west-1a"
+  cidr_block        = "10.0.0.0/16"
 }
