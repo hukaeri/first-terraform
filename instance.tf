9a85df58377488085f51e4f488ec97b9a6776650
xinyang.li <makeit.li@qq.com>
2024-02-28 15:22:57 +0800

    update action

--- instance.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_instance" "example" {
-  ami           = "ami-0905a3c97561e0b69"
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.main-public-1.id
-}
--- main.tf
+++ main.tf
@@ -1,4 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      Version = "~>3.27"
+    }
+  }
+
+  required_version = ">=0.14.9"
+
+  backend "s3" {
+    bucket = "tftestbucket"
+    key    = "path/to/my/key"
+    region = "eu-west-1"
+  }
+
+}
+
 provider "aws" {
-  region     = "eu-west-1"
-  profile    = "tw-beach"
+  version = "~>3.0"
+  region  = "eu-west-1"
 }
